186a7851ff3c613b043ba7a6f47c66ff9007a307
Harun Selman Karakaş <harunselmankarakas@gmail.com>
2025-03-12 19:14:50 +0300

    auth service's filter removed now...

--- auth/src/main/java/com/bookstore/auth/config/SecurityConfig.java
+++ auth/src/main/java/com/bookstore/auth/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.bookstore.auth.config;
 
-import com.bookstore.auth.filter.JwtFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,23 +7,20 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-  public static final String[] WHITELISTED_URLS = {"/api/v1/auth/**"};
+  private static final String[] WHITELISTED_URLS = {"/api/v1/auth/**"};
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtFilter jwtFilter)
-      throws Exception {
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     return http.csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(
             auth -> auth.requestMatchers(WHITELISTED_URLS).permitAll().anyRequest().authenticated())
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
         .build();
   }
 }
